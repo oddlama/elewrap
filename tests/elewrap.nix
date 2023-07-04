e6a43bb7c847aaeffdb6f08dd3d07bb95fb508b1
oddlama <oddlama@oddlama.org>
2023-07-05 00:43:55 +0200

    test: extended testing covering all features

--- tests/elewrap.nix
+++ tests/elewrap.nix
@@ -4,7 +4,12 @@
     self,
     pkgs,
     ...
-  }: {
+  }: let
+    showArguments = pkgs.writeShellScript "show-arguments" ''
+      ARGS=("$@")
+      printf "%s\n" "''${ARGS[*]@Q}"
+    '';
+  in {
     imports = [self.nixosModules.default];
     nixpkgs.overlays = [self.overlays.default];
 
@@ -15,11 +20,12 @@
       allowedUsers = ["test1"];
     };
 
-    # Test user change
+    # Test user change and allowedGroups
     security.elewrap.id-as-test2 = {
       command = ["${pkgs.coreutils}/bin/id"];
       targetUser = "test2";
       allowedUsers = ["test1"];
+      allowedGroups = ["group1"];
     };
 
     # Test user change with supplementary groups
@@ -30,20 +36,44 @@
     };
 
     # Ensure that extra arguments are ignored by default
+    security.elewrap.show-args-ignore-extra = {
+      command = [showArguments "--compiledArg1" "--compiledArg2"];
+      targetUser = "test1";
+      allowedUsers = ["test1"];
+    };
 
     # Ensure that extra arguments can be passed if explicitly allowed
+    security.elewrap.show-args-allow-extra = {
+      command = [showArguments "--compiledArg1" "--compiledArg2"];
+      targetUser = "test1";
+      allowedUsers = ["test1"];
+      passRuntimeArguments = true;
+    };
 
-    # Ensure that environment is empty by default
+    # Ensure that environment is forced empty by default
+    security.elewrap.show-env-allow-none = {
+      command = ["${pkgs.coreutils}/bin/env"];
+      targetUser = "test1";
+      allowedUsers = ["test1"];
+    };
 
     # Ensure that specific environment variables can be passed
+    security.elewrap.show-env-allow-var = {
+      command = ["${pkgs.coreutils}/bin/env"];
+      targetUser = "test1";
+      allowedUsers = ["test1"];
+      passEnvironment = ["VAR"];
+    };
 
     # Ensure that LD_PRELOAD is always removed (should be done by ld.so.1 due to CAP_SETUID)
+    security.elewrap.show-env-allow-ld = {
+      command = ["${pkgs.coreutils}/bin/env"];
+      targetUser = "test1";
+      allowedUsers = ["test1"];
+      passEnvironment = ["LD_PRELOAD"];
+    };
 
-    #security.elewrap.ensure-no-args-passwd = {
-    #  command = ["${pkgs.coreutils}/bin/id"];
-    #  targetUser = "root";
-    #  allowedUsers = ["test1"];
-    #};
+    # Ensure that the delimiter character does not appear in the command
 
     users.users.test1.group = "test1";
     users.users.test1.uid = 801;
@@ -63,24 +93,60 @@
     users.groups.group2.gid = 902;
   };
 
-  # TODO ensure test1 can execute all
-  # TODO test2 none
-  # TODO root none
-  # TODO ensure environment empty
-
   testScript = ''
     start_all()
 
     # wait for our service to start
     machine.wait_for_unit("multi-user.target")
 
-    output = machine.succeed("runuser -u test1 /run/wrappers/bin/elewrap-id-as-root")
-    assert output == "uid=0(root) gid=0(root) groups=0(root)\n"
+    def expect_output(output, expected):
+      assert output == expected, f"""
+        Expected output: {repr(expected)}
+        Actual output: {repr(output)}
+      """
 
-    output = machine.succeed("runuser -u test1 /run/wrappers/bin/elewrap-id-as-test2")
-    assert output == "uid=802(test2) gid=802(test2) groups=802(test2)\n"
+    output = machine.succeed("runuser -u test1 -- /run/wrappers/bin/elewrap-id-as-root")
+    expect_output(output, "uid=0(root) gid=0(root) groups=0(root)\n")
+    machine.fail("runuser -u root -- /run/wrappers/bin/elewrap-id-as-root")
+    machine.fail("runuser -u test2 -- /run/wrappers/bin/elewrap-id-as-root")
+    machine.fail("runuser -u test3 -- /run/wrappers/bin/elewrap-id-as-root")
 
-    output = machine.succeed("runuser -u test1 /run/wrappers/bin/elewrap-id-as-test3")
-    assert output == "uid=803(test3) gid=803(test3) groups=803(test3),901(group1),902(group2)\n"
+    output = machine.succeed("runuser -u test1 -- /run/wrappers/bin/elewrap-id-as-test2")
+    expect_output(output, "uid=802(test2) gid=802(test2) groups=802(test2)\n")
+    machine.fail("runuser -u root -- /run/wrappers/bin/elewrap-id-as-test2")
+    machine.fail("runuser -u test2 -- /run/wrappers/bin/elewrap-id-as-test2")
+    output = machine.succeed("runuser -u test3 -- /run/wrappers/bin/elewrap-id-as-test2")
+    expect_output(output, "uid=802(test2) gid=802(test2) groups=802(test2)\n")
+
+    output = machine.succeed("runuser -u test1 -- /run/wrappers/bin/elewrap-id-as-test3")
+    expect_output(output, "uid=803(test3) gid=803(test3) groups=803(test3),901(group1),902(group2)\n")
+    machine.fail("runuser -u root -- /run/wrappers/bin/elewrap-id-as-test3")
+    machine.fail("runuser -u test2 -- /run/wrappers/bin/elewrap-id-as-test3")
+    machine.fail("runuser -u test3 -- /run/wrappers/bin/elewrap-id-as-test3")
+
+    output = machine.succeed("runuser -u test1 -- /run/wrappers/bin/elewrap-show-args-ignore-extra")
+    expect_output(output, "'--compiledArg1' '--compiledArg2'\n")
+    output = machine.succeed("runuser -u test1 -- /run/wrappers/bin/elewrap-show-args-ignore-extra --extra1 --extra2")
+    expect_output(output, "'--compiledArg1' '--compiledArg2'\n")
+
+    output = machine.succeed("runuser -u test1 -- /run/wrappers/bin/elewrap-show-args-allow-extra")
+    expect_output(output, "'--compiledArg1' '--compiledArg2'\n")
+    output = machine.succeed("runuser -u test1 -- /run/wrappers/bin/elewrap-show-args-allow-extra --extra1 --extra2")
+    expect_output(output, "'--compiledArg1' '--compiledArg2' '--extra1' '--extra2'\n")
+
+    output = machine.succeed("runuser -u test1 -- sh -c '/run/wrappers/bin/elewrap-show-env-allow-none'")
+    expect_output(output, "")
+    output = machine.succeed("runuser -u test1 -- sh -c 'VAR=1 /run/wrappers/bin/elewrap-show-env-allow-none'")
+    expect_output(output, "")
+
+    output = machine.succeed("runuser -u test1 -- sh -c '/run/wrappers/bin/elewrap-show-env-allow-var'")
+    expect_output(output, "")
+    output = machine.succeed("runuser -u test1 -- sh -c 'VAR=1 /run/wrappers/bin/elewrap-show-env-allow-var'")
+    expect_output(output, "VAR=1\n")
+
+    output = machine.succeed("runuser -u test1 -- sh -c '/run/wrappers/bin/elewrap-show-env-allow-ld'")
+    expect_output(output, "")
+    output = machine.succeed("runuser -u test1 -- sh -c 'LD_PRELOAD=/lib/dummy.so /run/wrappers/bin/elewrap-show-env-allow-ld'")
+    expect_output(output, "")
   '';
 }
